fd879ba3553225031c63e9ce7aebd5ab3938d806
st3107 <st3107@columbia.edu>
2021-12-20 16:37:56 -0500

    MNT: remove conda-forge files and notebooks

--- notebooks/namespace.py
+++
@@ -1,8 +0,0 @@
-import dask
-import pandas as pd
-from databroker import Broker
-
-DB = Broker.named("xpd")
-UID = pd.read_csv("data/uid.csv")
-dask.config.set(scheduler='threads')
-DB2 = Broker.named("analysis")
