6c163f4787d9ad336610af1760023bb53b1e3159
st3107 <st3107@columbia.edu>
2022-01-03 15:12:30 -0500

    BLD: update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ read -p "Please give a name for the conda environment: " env
 echo "Download crystalmapping repo ..."
 git clone https://github.com/st3107/crystalmapping.git
 cd crystalmapping
-conda env create -n $env -f run-env.yaml
+conda env create -n $env
+conda run -n $env install --file ./requirements-run.txt
 conda run -n $env python3 -m pip install -e .
 echo "Package has been successfully installed."
