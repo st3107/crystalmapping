387c6bb0c879e3de88e6329fa25bb23c4c49b54b
st3107 <st3107@columbia.edu>
2022-06-15 22:44:07 -0700

    DEV: update readme and install

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 set -e
-read -p "Please give a name for the conda environment: " env
+read -p -r "Please give a name for the conda environment: " env
 echo "Download crystalmapping repo ..."
 git clone https://github.com/st3107/crystalmapping.git
 cd crystalmapping
-conda env create -n $env
-conda run -n $env install --file ./requirements-run.txt
-conda run -n $env python3 -m pip install -e .
+conda env create -n "$env" --yes
+conda run -n "$env" install --file ./requirements-run.txt --yes
+conda run -n "$env" python3 -m pip install -e .
 echo "Package has been successfully installed."
