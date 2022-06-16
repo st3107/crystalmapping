a63639d1f414f3be2eb304d561a1382b6ae8a8c0
Songsheng Tao <37090959+st3107@users.noreply.github.com>
2022-06-16 14:05:54 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 set -e
 read -p "Please give a name for the conda environment: " env
 echo "Download crystalmapping repo ..."
-conda env create -n "$env"
+conda create -n "$env" --yes
 conda install -n "$env" --file ./requirements-run.txt --yes
 conda run -n "$env" python3 -m pip install -e .
 echo "Package has been successfully installed."
