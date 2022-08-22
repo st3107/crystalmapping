76c235d409e4a5255fd3225b35d1a964bee6e947
st3107 <st3107@columbia.edu>
2022-08-22 13:22:39 -0400

    BUG: fix the bug in the stalllation on windows

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ read -p "Please give a name for the conda environment: " env
 echo "Download crystalmapping repo ..."
 conda create -n "$env" --yes
 conda install -n "$env" -c conda-forge -c defaults --file ./requirements-run.txt --yes
-conda run -n "$env" python3 -m pip install -e .
+conda run -n "$env" pip install -e .
 echo "Package has been successfully installed."
