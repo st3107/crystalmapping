7cc797b2b7f73fcce9f60f7a80417acede771170
st3107 <st3107@columbia.edu>
2022-11-18 11:43:14 -0500

    DEV: add git pull in update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 set -e
 env="$1"
+git pull
 conda install -n "$env" -c conda-forge -c defaults --file ./requirements-run.txt --yes
 conda run -n "$env" pip install -e .
 echo "Package has been successfully updated."
